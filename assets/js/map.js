8465acce71672c31913dceb34d502bacc340128d
ryan_p_larson <ryan_p_larson@github.com>
2016-09-21 14:01:09 -0500

    Queue implemented, loads both files correctly

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,27 +1,29 @@
+// Chart stuff
 var width = 960,
     height = 500;
-
-var projection = d3.geoMercator()
-    //.center([	-91.530167, 	41.661129]) //Iowa City long, lat
-    .center([-20, 20])
-    .scale(175)
-    .rotate([0, 0]);
 
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
 
+// Geo graphic stuff
+//[-91.530167, 41.661129] Iowa City
+var projection = d3.geoMercator()
+    .scale(150)
+    .translate([width / 2, height / 2]);
 var path = d3.geoPath()
+    .pointRadius(2)
     .projection(projection);
 
-var g = svg.append("g");
 
-// load and display the World
-d3.json("data/world-110m.json", function(error, topology) {
+function loadFiles(error, world, cyclocross) {
+    console.log(world);
+    console.log(cyclocross);
 
-    g.selectAll("path")
-      .data(topojson.feature(topology, topology.objects.countries).features)
-      .enter()
-      .append("path")
-      .attr("d", path);
-});
+}
+
+
+// Finally load the data and make the map
+d3.queue().defer(d3.json, 'data/world.json')
+        .defer(d3.csv, 'data/cyclocross.csv')
+        .await(loadFiles);
