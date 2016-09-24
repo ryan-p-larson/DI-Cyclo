647938df0eed3666cba99fb15d146a1917a5b3b4
ryan_p_larson <ryan_p_larson@github.com>
2016-09-23 20:10:32 -0500

    Chloropleth map and code

--- assets/js/map.js
+++ assets/js/map.js
@@ -2,8 +2,10 @@
 var width = 960,
     height = 500;
 
-var color = d3.scaleLinear()
-    .range(['#fed976','#feb24c','#fd8d3c','#f03b20','#bd0026']);
+// Manually set the color threshold because there's a mismatch
+var color = d3.scaleThreshold()
+    .domain([1, 4, 16, 20])
+    .range(['#b3cde3','#8c96c6','#8856a7','#810f7c']);
 
 var svg = d3.select("body").append("svg")
     .attr("width", width)
@@ -11,10 +13,10 @@ var svg = d3.select("body").append("svg")
 
 // Geographic stuff
 var projection = d3.geoMercator()
-    .scale(195)
+    .scale(175)
     // Make IC the middle
-    .rotate([91, 0])
-    .center([0, 21])
+    .rotate([110, 0])
+    .center([0, 15])
     .translate([width / 2, height / 2]);
 var path = d3.geoPath()
     .pointRadius(2)
@@ -23,57 +25,32 @@ var iowaCity = [-91.530167, 41.661129];
 
 
 // Interaction functions
-function mouseIn(country) {
-  d3.select(this)
-    .style('stroke-width', function(d) { return +d.Total; })
-    .style('stroke-opacity', 1);
-}
+// mouseIn
+// mouseOut
 
 // Drawing function
-function loadFiles(error, world, cyclocross) {
+function loadFiles(error, merged) {
 
-    // set the color domain based on the cyclocross entrants
-    color.domain([0, d3.max(cyclocross, function(d) { return +d.Total; })]);
+    // Save the nations in a var, Dont Repeat Yourself
+    var nations = topojson.feature(merged, merged.objects.countries).features;
 
+    // Filter to only entered nations, and sort the values in descending total
+    var cycloNations = nations
+            .filter(function(d) { return d.properties.Total; })
+            .sort(function(a, b) { return b.properties.Total - a.properties.Total; });
 
     // Add the map
-    svg.append('g').attr('class', 'map')
+    var map = svg.append('g').attr('class', 'map')
         .selectAll('path')
-            .data(topojson.feature(world, world.objects.countries).features)
+            .data(nations)
         .enter().append('path')
         // Give each country an ID (3 char abbreviation)
         .attr('id', function(d) { return d.id; })
-        .attr('d', path);
-
-    // Add the countries
-    svg.append('g').attr('class', 'cyclocross')
-        .selectAll('.circle')
-            .data(cyclocross)
-        .enter().append('circle')
-        .attr('id', function(d) { return d.Nation; })
-        .attr('transform', function(d) {
-                var center = projection([+d.Lon, +d.Lat]),
-                    x = center[0],
-                    y = center[1];
-                return 'translate(' + x + ', ' + y +')';
-        })
-        .attr('r', function(d) { return Math.sqrt(d.Total) * 1.5; });
-
-    // travel routes
-    svg.append('g').attr('class', 'travel')
-      .selectAll('.path')
-        .data(cyclocross)
-      .enter().append('path')
-      .attr('d', function(d) { return path({type: "LineString", coordinates: [[+d.Lon, +d.Lat], iowaCity]}); })
-      .style('stroke', '#fff')
-      .style('stroke-width', function(d) { return Math.sqrt(d.Total); })
-      .on('mouseover', mouseIn);
-
+        .attr('d', path)
+        .style('fill', function(d) { return d.properties.Total ? color(d.properties.Total) : '#ddd'; });
 }
-
 
 // Finally load the data and make the map
 d3.queue()
-    .defer(d3.json, 'data/world.json')
-    .defer(d3.csv, 'data/cyclocross.csv')
+    .defer(d3.json, 'data/merged.json')
         .await(loadFiles);
